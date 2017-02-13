fc164a34e1390a407a6f394f00969e84e43c3ca6
zeruiw2 <zeruiw2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-02-13 01:08:41 +0000

    lab_inheritance submission

--- lab_inheritance/flower.h
+++ lab_inheritance/flower.h
@@ -12,16 +12,17 @@
 
 class Flower : public Drawable
 {
-  private:
-    Shape stem;
-    Shape pistil; // center piece of flower
-    Shape leaf;
-
+public:
+    Shape* stem;
+    Shape* pistil; // center piece of flower
+    Shape* leaf;
+    void clear();
     void drawPetals(PNG* canvas, const Vector2& center, int x, int y) const;
 
-  public:
     Flower(const Vector2& center);
-    void draw(PNG* canvas) const;
+    void draw(PNG* canvas) const ;
+
+    virtual ~Flower();
 };
 
 #endif // FLOWER_H_
