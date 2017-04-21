c59f07ef4ed28c67c527ccc74b9f187531b5708e
zeruiw2 <zeruiw2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-04-21 01:55:36 +0000

    mp7 submission

--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,12 @@
 /* Your code here! */
+#include <vector>
+using namespace std;
+class DisjointSets{
+
+
+public:
+void addelements(int num);
+int  find(int elem);
+void setunion(int a, int b);
+std::vector<int> data;
+};
