e349fde79116332aa5cebb6d7c3bc9afa0fcbdf1
zeruiw2 <zeruiw2@1896fa9e-de69-4edd-9b00-c6635a71d013>
2017-05-02 04:30:39 +0000

    mp7 submission

--- zeruiw2/mp7/maze.h
+++ zeruiw2/mp7/maze.h
@@ -1,1 +1,28 @@
 /* Your code here! */
+#ifndef maze_H
+#define maze_H
+#include "png.h"
+#include <vector>
+#include "dsets.h"
+using namespace std;
+class SquareMaze{
+
+public:
+SquareMaze();
+void makeMaze(int width, int height );
+bool canTravel(int x, int y, int dir) const;
+void setWall(int x, int y, int dir, bool exists);
+PNG* drawMaze() const;
+PNG* drawMazeWithSolution();
+vector<int> solveMaze();
+private:
+int w;
+int h;
+vector<vector<bool>> rightlist;
+vector<vector<bool>> downlist;
+DisjointSets walls;
+
+
+};
+
+#endif
